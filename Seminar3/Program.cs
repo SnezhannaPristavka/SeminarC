54babd38ea0443a7477ad690b62a3ccd19d1be11
SnezhannaPristavka <gorlovasv41@mail.ru>
2022-03-03 10:39:55 +0300

    ghfgfd

---
+++ Seminar3/Program.cs
@@ -0,0 +1,15 @@
+﻿int GetQuart(double xPoint, double yPoint)
+{
+    if(xPoint > 0 && yPoint > 0) return 1;
+    if(xPoint < 0 && yPoint > 0) return 2;
+    if(xPoint < 0 && yPoint < 0) return 3;
+    if(xPoint > 0 && yPoint < 0) return 4;
+    if(xPoint == 0 || yPoint == 0) return 0;
+}
+
+double x,y;
+Console.Write ("Input x: ");
+x = Convert.ToDouble(Console.ReadLine());
+Console.Write ("Input y: ");
+y = Convert.ToDouble(Console.ReadLine());
+
